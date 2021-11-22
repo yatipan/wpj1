25a1156fe96502b4f9b7954b1dcfe6413715a762
yatipan <yatipanphyo.mh@gmail.com>
2021-11-22 11:15:24 +0630

    first commit

---
+++ record/models.py
@@ -0,0 +1,12 @@
+from typing import ContextManager
+from django.db import models
+
+# Create your models here.
+
+class Record(models.Model):
+    title = models.CharField(max_length=255)
+    Content = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
